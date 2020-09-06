79b153ee36f069c16b5b443220424023dffa6f6e
hcyoon <yhchco@gmail.com>
2020-09-06 15:40:39 +0900

    40. WEB2-PHP-27. 보안 XSS & 보안 파일 경로 보호

--- lib/print.php
+++ lib/print.php
@@ -1,7 +1,7 @@
 <?php
 function print_title(){
   if(isset($_GET['id'])) {
-    echo $_GET['id'];
+    echo htmlspecialchars($_GET['id']);
   } else {
     echo "Welcome";
   }
@@ -10,7 +10,8 @@ function print_title(){
 <?php
 function print_description(){
   if(isset($_GET['id'])) {
-    echo file_get_contents("data/".$_GET['id']);
+    $basename = basename($_GET['id']);
+    echo htmlspecialchars(file_get_contents("data/".$basename));
   } else {
     echo "Hello, PHP";
   }
@@ -21,9 +22,10 @@ function print_list(){
   $list = scandir('./data');
   $i = 0;
   while($i < count($list)) {
+    $title = htmlspecialchars($list[$i]);
     if($list[$i] != '.') {
       if($list[$i] != '..') {
-        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        echo "<li><a href=\"index.php?id=$title\">$title</a></li>\n";
       }
     }
     $i = $i + 1;
